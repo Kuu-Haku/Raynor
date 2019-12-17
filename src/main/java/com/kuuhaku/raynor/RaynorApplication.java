edb5863e1b8760a63c0ad752eef1033ec6c75d7e
kuuhaku <kuuhaku@msn.com>
2019-12-17 11:07:48 +0800

    Initial commit

---
+++ src/main/java/com/kuuhaku/raynor/RaynorApplication.java
@@ -0,0 +1,13 @@
+package com.kuuhaku.raynor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaynorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaynorApplication.class, args);
+    }
+
+}
